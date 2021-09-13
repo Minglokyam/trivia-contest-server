2c2df4f4c56afb14afe73f0e2f164b36d0d0fd2c
Your Name <you@example.com>
2021-09-13 19:35:20 -0400

    make it better

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import { UserResolver } from './resolvers/userResolver';
 
 const main = async () => {
     const app = express();
-    const port = 4000;
 
     await createConnections([{
         url: 'postgres://jilcswurwxfeor:fddc356fc93ae54a393e8745175578d69c05095ebef20d697a86d3f893b1cef6@ec2-52-0-93-3.compute-1.amazonaws.com:5432/dajs9255cl9b0',
@@ -34,7 +33,7 @@ const main = async () => {
 
     apolloServer.applyMiddleware({app});
 
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 4000, () => {
         console.log(`The server listening at http://localhost:${port}`)
     });
 }
